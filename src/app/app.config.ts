502703ca585931792b9a9c967ee312c45991479e
Mayar <mayardarwish21@gmail.com>
2024-10-27 20:10:25 +0300

    edit style

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig , LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) , provideHttpClient( withFetch() )]
+  providers: [provideRouter(routes) , provideHttpClient( withFetch() ),
+    {
+      provide: LOCALE_ID,
+      useValue: 'en' // 'de' for Germany, 'fr' for France ...
+     }
+  ]
 };
--- src/services/api-service.service.ts
+++ src/services/api-service.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { iProducts } from '../models/product.model';
@@ -6,12 +6,8 @@ import { iProducts } from '../models/product.model';
 @Injectable({
   providedIn: 'root'
 })
-export class ApiServiceService implements OnInit {
+export class ApiServiceService  {
   private apiurl = 'https://fakestoreapi.com/products?limit=10';
- ngOnInit(): void {
-     console.log('url' , environment);
-     
- }
 
   constructor( private _HttpClient:HttpClient ) { }
 
